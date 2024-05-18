6169f7787ae756830130a62f0886867df29c1712
Shawn Xu <xu107288696@gmail.com>
2024-05-18 03:19:36 -0700

    Reduce When TTValue is Above Alpha

--- src/search.cpp
+++ src/search.cpp
@@ -41,6 +41,7 @@
 #include "thread.h"
 #include "timeman.h"
 #include "tt.h"
+#include "types.h"
 #include "uci.h"
 #include "ucioption.h"
 
@@ -729,9 +730,12 @@ Value Search::Worker::search(
     if (PvNode && !ttMove)
         depth -= 2;
 
+    if (!PvNode && ss->ttHit && (tte->bound() & BOUND_UPPER) && ttValue > alpha + 5 * depth)
+        depth--;
+
     // Use qsearch if depth <= 0.
     if (depth <= 0)
-        return qsearch<PV>(pos, ss, alpha, beta);
+        return qsearch < PvNode ? PV : NonPV > (pos, ss, alpha, beta);
 
     // For cutNodes without a ttMove, we decrease depth by 2 if depth is high enough.
     if (cutNode && depth >= 8 && (!ttMove || tte->bound() == BOUND_UPPER))
